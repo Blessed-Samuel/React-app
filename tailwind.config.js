82839398827ebfc0295ccbd674cedc64df36d72b
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-09-19 10:11:29 +0100

    Created more pages.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./public/**/*.html"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwind-scrollbar"),
-  ],
+  plugins: [require("tailwind-scrollbar")],
 };
