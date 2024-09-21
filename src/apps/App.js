72705c7d36abb55ac0090000b1edb46dbef8977e
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-09-21 02:48:48 +0100

    Created app directory.

--- src/apps/App.js
+++ src/apps/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import logo from "./images/logo.svg";
-import Hero from "./components/Hero";
-import "./styles/App.css";
+import logo from "../images/logo.svg";
+import Hero from "../components/Hero";
+import "../styles/App.css";
 
 const App = () => {
   return (
